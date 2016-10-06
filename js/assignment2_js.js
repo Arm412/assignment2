316c375670056455452273b336cac24024f3bd3d
Adam Mitro <Arm148@pitt.edu>
2016-10-06 19:48:36 -0400

    NOW its completed (hopefully)

--- js/assignment2_js.js
+++ js/assignment2_js.js
@@ -1,7 +1,7 @@
 function display_name(name){
-    document.getElementById('error_text1').style.display = 'none';
+    document.getElementById('errorText1').style.display = 'none';
     if(name.length==0){
-        document.getElementById('error_text1').style.display = 'inline';
+        document.getElementById('errorText1').style.display = 'inline';
     }
     var header = document.getElementById('show_name')
     header.style.display = 'inline';
@@ -10,34 +10,56 @@ function display_name(name){
 }
 
 function change_background(color){
-    document.getElementById('error_text2').style.display = 'none';
+    document.getElementById('errorText2').style.display = 'none';
     if(color.length == 0){
-        document.getElementById('error_text2').style.display = 'inline';
+        document.getElementById('errorText2').style.display = 'inline';
     }
     document.getElementById('colors').style.backgroundColor = color;
     return;
 }
 
 function isItAHoverboard(answer){
-    document.getElementById('hidden_text_y').style.display = 'none';
-    document.getElementById('hidden_text_n').style.display = 'none';
-    document.getElementById('error_text').style.display = 'none';
+    document.getElementById('hiddenTextY').style.display = 'none';
+    document.getElementById('hiddenTextN').style.display = 'none';
+    document.getElementById('errorText').style.display = 'none';
     if(answer == "yes"){
-        document.getElementById('hidden_text_y').style.display = 'inline';
+        document.getElementById('hiddenTextY').style.display = 'inline';
     } else if(answer == "no"){
-        document.getElementById('hidden_text_n').style.display = 'inline';
+        document.getElementById('hiddenTextN').style.display = 'inline';
     } else {
-        document.getElementById('error_text').style.display = 'inline';
+        document.getElementById('errorText').style.display = 'inline';
     }
 }
 
-function disappear(){
+function disappear(name, color, answer){
+    a_ok=0;
+    if(name.length == 0){
+        display_name(name);
+        a_ok=1;
+    }
+    if(color.length == 0){
+        change_background(color);
+        a_ok=1;
+    }
+    if(answer.length == 0){
+        isItAHoverboard(answer);
+        a_ok=1;
+    }
+    if(a_ok==1){
+        return;
+    }
     document.getElementById('formHolder').style.display = 'none';
-    appear();
+    appear(name, color, answer);
 }
 
-function appear(){
-    document.getElementById('successDiv').style.display = 'block'
+function appear(name, color, answer){
+    if(answer == 'no'){
+        answer = 'Not a Hover board';
+    } else {
+        answer = 'Hover board'
+    }
+    document.getElementById('successDiv').style.display = 'block';
+    document.getElementById('showInput').innerHTML = 'Name: ' + name + '<br>Color: ' + color + '<br>Answer: ' + answer;
 }
 
 $(document).ready(function(){
